840175a9fe8cbe985661399ff20370725c431979
Teamthy <olusanyatimothy54@gmail.com>
2026-03-31 09:28:05 +0100

    demo for todolist app in go

--- demo.go
+++ demo.go
@@ -1,7 +1,17 @@
-package main 
+/*package main 
 
 import "fmt"
 
 func main() {
     fmt.Println("Hello, World!")
-} 
+}*/
+
+
+package main 
+
+import "fmt"
+
+func main () { 
+    fmt.Println("Welcome to my todolist app!")
+    fmt.Println("checkout my todolist")
+}
