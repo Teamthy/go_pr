f20ac5d034a105b086014d9cb205bb31cc09003e
Teamthy <olusanyatimothy54@gmail.com>
2026-04-01 23:29:43 +0100

    practice on functions in go

--- demo.go
+++ demo.go
@@ -15,3 +15,10 @@ func main () {
     fmt.Println("Welcome to my todolist app!")
     fmt.Println("checkout my todolist")
 }
+
+func main () {
+    x := 10
+    y := 20
+    sum := x + y
+    fmt.Println("The sum of x and y is:", sum)
+}
